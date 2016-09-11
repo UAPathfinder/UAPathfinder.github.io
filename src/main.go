12f18e31dc14ed3cdaec6a70abc6872434667c7d
mibzman <27thisotope@gmail.com>
2016-09-10 20:15:21 -0400

    loaded simple test data

--- src/main.go
+++ src/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-//"bufio"
-"fmt"
-//"os"
-//"database/sql" //non-functional MySQL imports
-// _ "/mysql"
+	//"bufio"
+	//"fmt"
+	"sort"
+	"time"
+	//"os"
+	//mysql --local-infile -uroot -pyourpwd yourdbname/"database/sql" //non-functional MySQL imports
+	// _ "/mysql"
 )
 
 type Class struct { //A single class
 	//ex Data Structures starting at 3 PM in room 301 with professor x
 	ClassId         int
 	CourseId        int
-	StartTime       string //ISO 8601 (hh:mm:ss) e.x. 12:15:00
-	EndTime         string //see above
+	StartTime       time.Time
+	EndTime         time.Time
 	MeetingDays     string //e.x. MWF, TH (Tuesday tHursday)
 	ProfessorName   string
 	MeetingLocation string
@@ -23,16 +25,44 @@ type Course struct { //ex 3960:401 Data Structures
 	CourseId  int
 	Priority  int //from 1 to 10
 	Manditory bool
-	Classes []Class//initializing an empty slice
+	Classes   []Class //initializing an empty slice
 }
+
+type Combo struct {
+	Classes []Class
+	Score   int
+}
+
+type Criteria struct {
+}
+
+type ByScore []Combo                 //implements sort.Interface for []Combo based on Score
+func (a ByScore) Len() int           { return len(a) }
+func (a ByScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByScore) Less(i, j int) bool { return a[i].Score < a[j].Score }
 
 func main() {
-	
 }
 
-func FillCourses(courses map[int]int){
+func NumCombos(courses []Course) int { //untested, waiting for data
+	var total int = 1
 	for i := range courses {
-		fmt.Println(courses[i])
+		total *= len(courses[i].Classes)
+	}
+	return total
+}
+
+func ScoreCombo(combo Combo) {
+
+}
+
+func OrderCombos(combos []Combo) {
+	sort.Sort(ByScore(combos))
+}
+
+func FillCourses(courses map[int]int) { //has to wait for sql stuff
+	for i := range courses {
+		courses[i]++
 	}
 }
 
@@ -51,7 +81,7 @@ func MySQLQuery(input string) string {
 	return "not initialized"
 }
 
-func GetCourses() map[int]int {
+func GetCourses() map[int]int { //has to wait for sql stuff
 	//retrives list of course id's with priorities
 	output := make(map[int]int)
 	output[1] = 2 //tempoary output for testing
--- src/main_test.go
+++ src/main_test.go
@@ -1,15 +1,58 @@
 package main
 
 import (
-"testing"
+	"testing"
+	"time"
 )
 
 
-func TestGetCourses(t *testing.T){
+func TestGetCourses(t *testing.T) {
 	output := GetCourses()
 	value := output[1]
-	if value != 2{
+	if value != 2 {
 		t.Fatalf("Get Courses is not returning the testing output: output[1] = 2")
-	} 
+	}
 }
 
+func TestFillCourses(t *testing.T) {
+	input := map[int]int{
+		1: 1,
+		2: 2,
+		3: 3,
+	}
+	FillCourses(input)
+	if input[1] != 2 {
+		t.Fatalf("output is not correct.  output[1] = %d", input[1])
+	}
+}
+
+type Flex struct { //allowes for a mixed array
+	f byte
+}
+
+func TestNumCombos(t *testing.T) {
+class1 := Class{ClassId:1,CourseId: 1,StartTime: SimpleParse("09:00:00"),EndTime: SimpleParse("10:00:00"),MeetingDays: "MWF",ProfessorName: "Bob Jones",MeetingLocation: "Leigh Hall"}
+//The sample data is all the same prof and location because we're not testing those portions right now
+class2 := Class{ClassId:2,CourseId: 1,StartTime: SimpleParse("11:30:00"),EndTime: SimpleParse("13:00:00"),MeetingDays: "TH",ProfessorName: "Bob Jones",MeetingLocation: "Leigh Hall"}
+class3 := Class{ClassId:3,CourseId: 1,StartTime: SimpleParse("05:05:00"),EndTime: SimpleParse("06:15:00"),MeetingDays: "MW",ProfessorName: "Bob Jones",MeetingLocation: "Leigh Hall"}
+class4 := Class{ClassId:4,CourseId: 2,StartTime: SimpleParse("09:25:00"),EndTime: SimpleParse("10:45:00"),MeetingDays: "THF",ProfessorName: "Bob Jones",MeetingLocation: "Leigh Hall"}
+class5 := Class{ClassId:5,CourseId: 2,StartTime: SimpleParse("07:45:00"),EndTime: SimpleParse("10:00:00"),MeetingDays: "MW",ProfessorName: "Bob Jones",MeetingLocation: "Leigh Hall"}
+class6 := Class{ClassId:6,CourseId: 2,StartTime: SimpleParse("18:00:00"),EndTime: SimpleParse("19:45:00"),MeetingDays: "TH",ProfessorName: "Bob Jones",MeetingLocation: "Leigh Hall"}
+
+var arr1 = []Class{class1, class2, class3}
+var arr2 = []Class{class4, class5, class6}
+
+course1 := Course{CourseId: 1, Priority: 9, Manditory: true, Classes: arr1}
+course2 := Course{CourseId: 2, Priority: 7, Manditory: false, Classes: arr2}
+var arr3 = []Course{course1, course2}
+output := NumCombos(arr3)
+if output != 9{
+	t.Fatalf("NumCombos is not correct.  output should be 9, is: %d", output)
+}
+
+}
+
+func SimpleParse(input string) time.Time{
+        output, _ := time.Parse("15:04:05", input)
+        return output
+}
