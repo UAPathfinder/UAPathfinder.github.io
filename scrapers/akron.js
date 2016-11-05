cd1358143e23ba5cb2044b6752102221ade81d5f
Martin Charles <martincharles07@gmail.com>
2016-11-05 00:07:48 -0400

    Scraper Progress
    Data scraping part is done. Just need to figure out a data model, framework
    structure for the CLI and extend to all courses.

--- scrapers/akron.js
+++ scrapers/akron.js
@@ -3,7 +3,7 @@ var webdriver = require('selenium-webdriver'),
     until = webdriver.until;
 
 var driver = new webdriver.Builder()
-	.usingServer('http://localhost:4444/wd/hub')
+	.usingServer('http://192.168.1.217:4444/wd/hub')
     .forBrowser('firefox')
     .build();
 
@@ -32,7 +32,7 @@ var options = driver.findElements(By.css("select[id^=SSR_CLSRCH_WRK_SUBJECT_] > 
 	.then(departments => departments.forEach((department) => {
 		Promise.all([departmentSelector, department])
 			.then(values => {
-				console.log(values[1]);
+				console.log(`Department: ${values[1]}`);
 				return driver.executeScript(
 					(selector, value) => {
 						selector.value = value;
@@ -58,12 +58,68 @@ var options = driver.findElements(By.css("select[id^=SSR_CLSRCH_WRK_SUBJECT_] > 
 			// Get Courses
 			.then(() => driver.findElements(By.css(coursesSelector)))
 			.then(courses => courses.forEach(course => {
+				// Course Title
 				course.findElement(By.css('.PAGROUPBOXLABELLEVEL1'))
 					.then(title => title.getText())
-					.then(titleText => console.log(titleText));
+					.then(titleText => console.log(titleText.trim()));
 
-					// TODO: Extract Sections
-					// TODO: EXtract Data
+				// Course Sections
+				course.findElements(By.css(sectionsSelector))
+					.then(sections => sections.forEach(section => {
+						// Class Number
+						section.findElement(By.css('a[id^=MTG_CLASS_NBR]'))
+							.then(numberNode => numberNode.getText())
+							.then(numberText => console.log(`Class Number: ${numberText}`));
+
+						// Section Name
+						section.findElement(By.css('a[id^=MTG_CLASSNAME]'))
+							.then(sectionNode => sectionNode.getText())
+							.then(section => console.log(`Section: ${section.replace('\n', ' ')}`))
+
+						// Class Time + Date
+						// Examples:
+						// - Th 1:10PM - 2:50PM
+						// - MoWe 1:10PM - 2:25PM
+						// - TuTh 5:10PM - 6:00PM
+						// - MoWeFr 8:50AM - 12:30PM
+						section.findElement(By.css('[id^=MTG_DAYTIME]'))
+							.then(timeNode => timeNode.getText())
+							.then(timeText => console.log(`Times: ${timeText}`));
+
+						// Room
+						section.findElement(By.css('[id^=MTG_ROOM]'))
+							.then(locNode => locNode.getText())
+							.then(location => console.log(`Room: ${location}`));
+
+						// Instructor
+						section.findElement(By.css('[id^=MTG_INSTR]'))
+							.then(instructorNode => instructorNode.getText())
+							.then(instructor => console.log(`Instructor: ${instructor}`));
+
+						// Units
+						section.findElement(By.css('[id^=UA_DERIVED_SRCH_DESCR2'))
+							.then(unitsNode => unitsNode.getText())
+							.then(units => console.log(`Units: ${units}`));
+
+						// Enrolled
+						section.findElement(By.css('[id^=UA_DERIVED_SRCH_DESCR3'))
+							.then(enrolledNode => enrolledNode.getText())
+							.then(enrolled => console.log(`Enrolled: ${enrolled}`))
+							.then(_ => console.log("\n"));
+
+						// TODO: Textbook
+						// When the textbook link is clicked, javascript submits
+						// a form which responds with some html. The html is
+						// inlined and script tags in the response executed. The
+						// code which opens the window for textbook information
+						// is somewhere in these script tags.
+
+						// TODO: Status
+						// Not sure what this means because even last semesters
+						// classes are green. They can't be registered for.
+						// Maybe this is for prereqs?
+					}));
+
 					// TODO: Commit to Rethink
 					// TODO: Callback hell might have been better.
 			}))
