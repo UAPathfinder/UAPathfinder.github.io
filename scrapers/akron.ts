28556fd2a107976d523abb6d16a4ec5258929428
Martin Charles <martincharles07@gmail.com>
2016-11-08 20:07:44 -0500

    Added Database and Some Parsing
    Times are now parsed. Date's aren't.

--- scrapers/akron.ts
+++ scrapers/akron.ts
@@ -1,4 +1,13 @@
+import "reflect-metadata";
+
 import { By, until, Builder } from 'selenium-webdriver';
+import { createConnection } from "typeorm";
+
+import { Course } from './entity/course';
+import { Class } from './entity/class';
+import { Department } from './entity/department';
+
+import { Time } from './time';
 
 // Get Cookies
 const url1 = "https://my.uakron.edu/psp/portprodg/EMPLOYEE/CS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL";
@@ -21,11 +30,34 @@ const coursesSelector = [
 	"td", "table", "tbody", "tr:not(:first-child)"
 ].join(' > ');
 
+// const coursesSelector = [
+// 	'#ACE_$ICField$12$$0'.replace('$', '\\$'),
+// 	'tbody', 'tr:not(:first-child)'
+// ].join(' > ');
+
+// TODO: Optimize Selectors
 const sectionsSelector = [
 	"[id^=ACE_SSR_CLSRSLT_WRK_GROUPBOX2]", "tbody", "tr:nth-child(2)",
 	"td:last-child", "div", "table", "tbody", "tr", "td", "table", "tbody",
 	"tr:nth-child(even)"
 ].join(' > ');
+
+var db;
+const dbPromise = createConnection({
+	driver: {
+		type: "sqlite",
+		storage: "../data/akron.db",
+	},
+	logging: {
+		logQueries: true,
+		logFailedQueryError: true,
+		logOnlyFailedQueries: true,
+		logSchemaCreation: true,
+	},
+	entities: [
+		Course, Class, Department,
+	],
+});
 
 var driver = new Builder()
 	// .usingServer('http://devbook.lan:4444/wd/hub')
@@ -41,8 +73,10 @@ async function getMetadata() {
 
 	const departmentSelector = await driver.findElement(By.css("select[id^=SSR_CLSRCH_WRK_SUBJECT_]"));
 	const departments = (await departmentSelector.findElements(By.css('option'))).length;
+	db = await dbPromise;
 
 	for (let i = 2; i < departments; i++) {
+		console.log(`Progress ${i}/${departments}`);
 		await handleDepartment(i);
 		await driver.get(url2)
 	}
@@ -51,11 +85,22 @@ async function getMetadata() {
 async function handleDepartment(i: number) {
 	const departmentSelector = await driver.findElement(By.css("select[id^=SSR_CLSRCH_WRK_SUBJECT_]"));
 	const departmentOption = await departmentSelector.findElement(By.css(`option:nth-child(${i})`))
-	const departmentName = await departmentOption.getText();
+	const departmentName = await departmentOption.getText()
+		.then(text => text.split(/\s{4}/)[0])
+		// Example:
+		//     Divorce Mediation    (1800)
+		//     ->
+		//     Divorce Mediation
+
+	const department = await departmentOption.getAttribute('value');
 
 	console.log(departmentName);
 
-	const department = await departmentOption.getAttribute('value');
+	let deptModel = new Department();
+	deptModel.title = departmentName;
+	deptModel.identifier = department;
+
+	const deptModelPromise = db.entityManager.persist(deptModel);
 
 	// Select Department
 	await driver.executeScript((selector, value) => {
@@ -91,9 +136,28 @@ async function handleDepartment(i: number) {
 
 	for (let course of courses) {
 		// Course Title
+		// Example:
+		//     " 7400 123 - Fundamentals of Construction " ->
+		//     ["7400 123", "Fundamentals of Construction"]
+
+		// TODO: Batch these.
 		const coursePromise = course.findElement(By.css('.PAGROUPBOXLABELLEVEL1'))
 			.then(title => title.getText())
-			.then(titleText => titleText.trim());
+			.then(titleText => titleText.trim().replace(/\s+/g, ' ').split(' - '))
+			// Persist
+			.then(async information => {
+				await deptModelPromise;
+				let [identifier, title] = information;
+				let course = new Course();
+				course.department = department;
+				course.identifier = identifier;
+				course.title = title;
+				// TODO: Description
+				// TODO: Units
+
+				db.entityManager.persist(course);
+				return information;
+			});
 
 		// Course Sections
 		var sections = await course.findElements(By.css(sectionsSelector));
@@ -114,10 +178,55 @@ async function handleDepartment(i: number) {
 			// - MoWe 1:10PM - 2:25PM
 			// - TuTh 5:10PM - 6:00PM
 			// - MoWeFr 8:50AM - 12:30PM
-			//
-			// TODO: Parse
 			var daytimePromise = section.findElement(By.css('[id^=MTG_DAYTIME]'))
-				.then(timeNode => timeNode.getText());
+				.then(timeNode => timeNode.getText())
+				.then(text => {
+					if (text == "TBA")
+						return null;
+
+					// Handle Days
+					var days = {
+						"monday": false,
+						"tuesday": false,
+						"wednesday": false,
+						"thursday": false,
+						"friday": false,
+						"saturday": false,
+						"sunday": false,
+					};
+
+					const mapping = {
+						"Mo": "monday",
+						"Tu": "tuesday",
+						"We": "wednesday",
+						"Th": "thursday",
+						"Fr": "friday",
+
+						// Can't confirm if these last few are right. Haven't seen
+						// them in the wild.
+						"Sa": "saturday",
+						"Su": "sunday",
+					};
+
+					var i;
+					for (i = 0; i < text.length && text[i] != ' '; i += 2) {
+						let rawDay = text.substr(i, 2);
+						let rawToNice = mapping[rawDay];
+						if (rawToNice) {
+							days[rawToNice] = true;
+							delete mapping[rawDay];
+						} else {
+							// Duplicate days or some wired string.
+							return null;
+						}
+					}
+
+					let times = text.substr(i + 1)
+						.split(' - ')
+						.map(time => new Time(time).toSeconds());
+
+					return [days, times];
+				});
 
 			// Room
 			var roomPromise = section.findElement(By.css('[id^=MTG_ROOM]'))
@@ -140,43 +249,56 @@ async function handleDepartment(i: number) {
 				coursePromise, numberPromise, sectionNamePromise, daytimePromise,
 				roomPromise, instructorPromise, unitsPromise, enrolledPromise
 			]).then(resolved => {
-				let [course, sectionNumber, sectionName, daytime, room, instructor,
-					units, enrolled] = resolved;
+				let [[courseIdentifier, courseTitle], sectionNumber, sectionName,
+					daysTimes, room, instructor, units, enrolled] = resolved;
 
-				return {
-					id: sectionNumber,
-					course: course,
-					daytime: daytime, // TODO: Parse
-					location: room,
-					instructor: instructor,
-					units: units,
-					enrolled: enrolled,
-				};
-			})
+				// TODO: Move units to course.
+
+				let klass = new Class();
+				klass.identifier = sectionNumber;
+				klass.course = courseIdentifier;
+				klass.location = room;
+				// TODO: professor -> instructor?
+				klass.professor = instructor;
+				klass.registered = enrolled;
+
+				if (daysTimes) {
+					let [days, times] = daysTimes;
+					// TODO: wtf is this case?
+
+					if (times) {
+						[klass.start_time, klass.end_time] = times
+					}
+
+					// Copy Days (sunday-saturday)
+					Object.assign(klass, days);
+				}
+
+				// TODO: start_date, end_date
+				return klass;
+			});
 
 			sectionPromises.push(sectionPromise);
 
-			// TODO: Meeting Days
 			// TODO: Textbook
 			// When the textbook link is clicked, javascript submits a form
 			// which responds with some html. The html is inlined and script
 			// tags in the response executed. The code which opens the window
 			// for textbook information is somewhere in these script tags. a
 
-			// TODO: Status
-			// Not sure what this means because even last semesters classes are
-			// green. They can't be registered for. Maybe this is for prereqs?
-
-			// TODO: Commit to Rethink
-
 			// TODO: After 103 minutes of running, the page refreshes
 			// spontaneously and the script breaks. Element references fail to
 			// resolve and the script crashes. I believe the scraper got kicked
 			// due to in-activity on the page (15 minutes?). Section after
 			// writing.
+
+			// TODO: Do Something About Database Conflicts
 		}
 	}
 
-	var sections = await Promise.all(sectionPromises)
-	console.log(sections);
+	var classes = await Promise.all(sectionPromises)
+	if (classes.length != 0)
+		db.entityManager.persist(classes);
+
+	console.log(classes);
 }
